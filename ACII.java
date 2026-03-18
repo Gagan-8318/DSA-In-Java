87c81816979518e57052ccc448c96cc84cab2fb0
Gagan Bhadauriya <gaganbhadauriya5max@gmail.com>
2026-03-18 11:43:10 +0530

    ascii

---
+++ ACII.java
@@ -0,0 +1,14 @@
+class solution{
+    void show(){
+        char c= 'A';
+        System.out.println("the ascii value"+c+" is "+ (int) c);
+
+    }
+}
+public class ACII {
+
+    public static void main(String[] args) {
+        solution s=new solution();
+        s.show();
+    }
+}
